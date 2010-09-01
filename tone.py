291882efa5b2088c36fcb9218f509652821309b5
Raphael Wimmer <raphael.wimmer@ifi.lmu.de>
2010-09-01 03:33:52 +0200

    wip tone.py

--- tone.py
+++ tone.py
@@ -3,16 +3,19 @@
 from pyeca import *
 
 class Synth:
-    def __init__(self, freq=440):
+    def __init__(self, max_channels=1):
         self.e = ECA_CONTROL_INTERFACE()
         self.e.command("cs-add play_chainsetup")
-        self.e.command("c-add 1st_chain")
-        self.e.command("ai-add null")
-        self.e.command("ao-add /dev/dsp")
-        self.e.command("cop-add el:sine_fcac,"+ str(freq) + ",1")
+        selected=""
+        for i in range(max_channels):
+           self.e.command("c-add sin"+str(i))
+           #self.e.command("c-select sin"+str(i))
+           self.e.command("ai-add null")
+           self.e.command("ao-add /dev/dsp")
+           print self.e.command("c-selected")
+           self.e.command("cop-add el:sine_fcac,440,1")
+        
         self.e.command("cs-connect")
-        self.e.command("cop-select 2")
-        self.e.command("copp-select 1")
 
     def start(self):
         self.e.command("start")
@@ -20,18 +23,20 @@ class Synth:
     def stop(self):
         self.e.command("stop")
 
-    def set(self, freq):
+    def set(self, channel, freq):
+        self.e.command("c-select sin"+str(channel))
+        self.e.command("cop-select 2")
+        self.e.command("copp-select 1")
         self.e.command("copp-set " + str(freq))
 
 
 if __name__ == "__main__":
     import time
     import sys
-    s = Synth()
-    if len(sys.argv) > 1:
-        s.set(int(sys.argv[1]))
-    else:
-        s.set(440)
+    s = Synth(2)
     s.start()
+    time.sleep(2)
+    s.set(0,440)
+    s.set(1,330)
     time.sleep(2)
     s.stop()
