1969fd2441d12bd317728b39541393dabdcd93c2
Raphael Wimmer <raphael.wimmer@ifi.lmu.de>
2010-12-01 05:57:17 +0100

    use undistort filter in VideoSource

--- VideoSource.py
+++ VideoSource.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import cv
+from undistort import *
 
 class VideoSource:
     def __init__(self):
@@ -8,9 +9,11 @@ class VideoSource:
         cv.SetCaptureProperty(self.capturing_device, cv.CV_CAP_PROP_FPS, 30)
         self.img = None
         #self.imageColor = cv.CreateImage([640,480], cv.IPL_DEPTH_8U, 3)
+        init_undistort(interactive=True)
 
     def next(self):
         self.img = cv.QueryFrame(self.capturing_device)
+        self.img = undistort(self.img)
         #cv.CvtColor(self.img, self.imageColor, cv.CV_GRAY2RGB)
         return self.img
         
